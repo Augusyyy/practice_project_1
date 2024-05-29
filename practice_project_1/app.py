714ad5befdbc68572f23eefea1f63a109ab5df4f
Augusyyy <8851@holbertonstudents.com>
2024-05-29 17:01:50 +1000

    step6

--- practice_project_1/app.py
+++ practice_project_1/app.py
@@ -4,6 +4,8 @@ from model.city import City
 from data import place_data
 from data import amenity_data
 from data import place_to_amenity_data
+from data import user_data
+from data import review_data
 
 
 app = Flask(__name__)
@@ -36,6 +38,63 @@ def example_cities():
 @app.route('/example/places_amenities_raw', methods=['GET'])
 def example_places_amenities_raw():
     return jsonify(place_to_amenity_data)
+
+
+@app.route('/example/places_amenities_prettified_example', methods=['GET'])
+def example_places_amenities_prettified():
+    place_list = place_data['Place']
+    amenity_list = amenity_data['Amenity']
+    place_to_amenity_list = place_to_amenity_data['Place_to_Amenity']
+
+    output = {}
+    data = []
+
+    for place_to_amenity_item in place_to_amenity_list:
+
+        place_id = place_to_amenity_item["place_id"]
+        amenity_id = place_to_amenity_item["amenity_id"]
+
+        sub_out_put = {}
+
+        for amenity_item in amenity_list:
+            if amenity_item['id'] == amenity_id:
+                sub_out_put['amenity'] = amenity_item
+
+        for place_item in place_list:
+            if place_item['id'] == place_id:
+                sub_out_put['place'] = place_item
+
+        data.append(sub_out_put)
+    output['data'] = data
+    return jsonify(output)
+
+@app.route('/example/places_reviews', methods=['GET'])
+def example_places_reviews():
+    review_list = review_data['Review']
+    place_list = place_data['Place']
+    user_list = user_data['User']
+
+    en_reviews = []
+
+    for review_item in review_list:
+
+        place_id = review_item["place_id"]
+        user_id = review_item["user_id"]
+
+        sub_out_put = {}
+
+        for place_item in place_list:
+            if place_item['id'] == place_id:
+                sub_out_put['place'] = place_item
+
+        for user_item in user_list:
+            if user_item['id'] == user_id:
+                sub_out_put['user'] = user_item
+
+        en_reviews.append(sub_out_put)
+
+    return jsonify(en_reviews)
+
 
 
 
--- practice_project_1/data/__init__.py
+++ practice_project_1/data/__init__.py
@@ -9,3 +9,7 @@ place_data = storage.load_model_data('data/place.json')
 amenity_data = storage.load_model_data('data/amenity.json')
 
 place_to_amenity_data = storage.load_model_data('data/place_to_amenity.json')
+
+user_data = storage.load_model_data('data/user.json')
+
+review_data = storage.load_model_data('data/review.json')
